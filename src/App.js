bab7d629789f83ed4945f29aa68c361548673c33
RIfat Shikder <rifatptk@gmail.com>
2022-04-07 14:47:46 +0600

    some UI changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Profile from "./PortfolioContainer/Home/Profile";
 import Navigation from "./PortfolioContainer/Navigation/Navigation";
 import Skills from "./PortfolioContainer/Skills/Skills";
+import AboutMe from "./PortfolioContainer/AboutMe/AboutMe";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation/>
       <Profile />
       <Skills/>
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./AboutMe.css";
+
+export default function AboutMe() {
+  return (
+    <div className="aboutme-container">
+      <h1>
+        About<span className="aboutme-white">Me</span>
+      </h1>
+      <h5>Get to know me</h5>
+      <p>
+        My name’s Rifat, I’m 26, have worked as a software engineer for a smaller
+        company, in an agile team, working mostly on online maps, with JavaScript and
+        Python. But just like many other startups this one eventually hasn’t taken
+        of, and now I find myself unemployed. I prefer to work in a smaller
+        team, and see the direct impact I have on a project, which is the reason
+        no. 1 why I am applying for the job with you. Other than that I am more
+        or less an ordinary guy, with a girlfriend and a dog, and a passion for
+        model trains. Is there anything else you’d like to know about my life at
+        the moment?
+      </p>
+    </div>
+  );
+}
